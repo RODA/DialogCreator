7d7a02a1096b43999228c9085a8fc36b68e4e9a7
Emilian Adrian Hossu <emil.hossu@gmail.com>
2023-10-02 21:26:01 +0200

    add element to dialog

--- src/editor/editor.ts
+++ src/editor/editor.ts
@@ -1,38 +1,41 @@
 import { EventEmitter } from 'events';
 import { editorSettings } from './settings';
-import * as elements from './elements/index';
+import * as elementsConfig from './elements/index';
+import { elementsInterfaces } from './elements/index';
 import { v4 as uuidv4 } from 'uuid';
-
-
-
+import { editorElements, editorElementsTypes } from './editorElements';
+console.log(editorElements);
 interface EditorInterface {
+    dialog: HTMLDivElement;
     dialogId: string;
     editorEvents: EventEmitter;
     make: (dialogContainer: HTMLDivElement) => void;
     drawAvailableElements: () => HTMLUListElement;
     // deselectAll: () => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    addElementToDialog: (type: string, withData?: any) => void;
 }
 
 export const editor: EditorInterface = {
 
+    dialog: document.createElement('div'),
     dialogId: '',
     editorEvents: new EventEmitter(),
-
     make: (dialogContainer: HTMLDivElement) => {
-        
-        const dialog = document.createElement('div');
+
         const newDialogID = uuidv4();
-        dialog.id = newDialogID;
+        editor.dialog.id = newDialogID;
         editor.dialogId = newDialogID;
 
-        dialog.style.width = editorSettings.dialog.width + 'px';
-        dialog.style.height = editorSettings.dialog.height + 'px';
-        dialog.style.backgroundColor = editorSettings.dialog.background;
-        dialog.style.border = '1px solid gray';
+        editor.dialog.style.position = 'relative';
+        editor.dialog.style.width = editorSettings.dialog.width + 'px';
+        editor.dialog.style.height = editorSettings.dialog.height + 'px';
+        editor.dialog.style.backgroundColor = editorSettings.dialog.background;
+        editor.dialog.style.border = '1px solid gray';
         // dialog.addEventListener('click', editor.deselectAll);
 
-        dialogContainer.append(dialog)
-        
+        dialogContainer.append(editor.dialog)
+
         // // bg id for resize
         // this.bgId = bgRect.id;
         // // set paper exists
@@ -50,11 +53,53 @@ export const editor: EditorInterface = {
         ul.setAttribute('id', 'paperAvailableElements');
         for (const element of editorSettings.availableElements) {
             const li = document.createElement('li');
-            li.setAttribute('id', element);
+            li.setAttribute('id', uuidv4());
             li.innerHTML = element;
+            li.addEventListener('click', () => {
+                editor.addElementToDialog(element, null);
+            })
             ul.appendChild(li);
         }
         return ul;
+    },
+
+    // add new element on paper
+    addElementToDialog: function (type, withData) {
+        // checking if there is a paper
+        if (editor.dialog) {
+
+
+            if (!editorSettings.availableElements.includes(type) || !Object.hasOwn(editorElements, 'add' + type)) {
+                alert("Element type not available. Probably functionality not added.");
+                return;
+            }
+
+            let dataSettings;
+
+            if (withData !== null) {
+                dataSettings = withData;
+            } else {
+                const aa = (type.toLowerCase() + 'Element') as elementsInterfaces;
+                dataSettings = elementsConfig[aa];
+            }
+
+            // checking for duplicate names | checking for the name propertie if exist should not be necessary as all elements should have it           
+            // if (Object.hasOwn(dataSettings, 'name')) {
+            //     dataSettings.name = container.elementNameReturn(dataSettings.name);
+            // }
+
+            // check for wrong values
+            // dataSettings = container.cleanValues(dataSettings);
+
+            editorElements['add' + type as editorElementsTypes](editor.dialog, dataSettings);
+
+            // adn cover, drag and drop and add it to the container
+            // this.addCoverAndDrag(element, dataSettings, false);
+
+        } else {
+            // dialog.showMessageBox(editorWindow, { type: "info", message: "Please create a new dialog first.", title: "No dialog", buttons: ["OK"] });
+            alert('Please create a new dialog first.');
+        }
     },
 
     // deselectAll: function()
--- src/editor/elements/index.ts
+++ src/editor/elements/index.ts
@@ -3,3 +3,6 @@ export { checkboxElement, checkboxElementType } from './checkbox';
 export { counterElement, counterElementType } from './counter';
 export { containerElement, containerElementType } from "./container";
 export { inputElement, inputElementType } from "./input";
+
+
+export type elementsInterfaces = 'buttonElement' | 'checkboxElement';
