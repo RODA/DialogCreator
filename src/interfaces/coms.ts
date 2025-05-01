bfe028c8154304838a09b5cccd599b28d8df98f2
Adrian Dușa <dusa.adrian@gmail.com>
2025-05-01 23:29:46 +0300

    more refined Global object

--- src/interfaces/coms.ts
+++ src/interfaces/coms.ts
@@ -1,6 +1,20 @@
+import { EventEmitter } from 'events';
+import { Elements } from './elements';
 
 export interface ShowMessage {
     type: 'info' | 'error' | 'question' | 'warning';
     title: string;
     message: string;
+}
+
+export interface Global {
+    messenger: EventEmitter;
+    elements: Elements;
+    elementSelected: boolean;
+    handlers: {
+        [key: string]: {
+            module: string;
+            functioname: string;
+        }
+    }
 }
--- src/modules/coms.ts
+++ src/modules/coms.ts
@@ -1,18 +1,42 @@
 // encapsulation
 
 import { ipcRenderer } from 'electron';
-import { ShowMessage } from '../interfaces/coms';
+import { ShowMessage, Global } from '../interfaces/coms';
 import { EventEmitter } from 'events';
+import { Elements } from '../interfaces/elements';
+import { elements } from '../modules/elements';
+// import { utils } from '../library/utils';
 
-export const global = {
-    emitter: new EventEmitter()
+export const global: Global = {
+    messenger: new EventEmitter(),
+    elements: {} as Elements,
+    elementSelected: false,
+    handlers: {
+        populateDefaults: {
+            module: 'defaults', // the handler auto-finds the module in the modules/ folder
+            functioname: 'addElementsToDefaults'
+        },
+        // add more events here
+    }
 }
+
+global.elements = { ...elements };
+global.messenger.on('updateDefaults', (event, updatedDefaults) => {
+    global.elements = { ...updatedDefaults };
+});
+
+// for (const eventName in global.handlers) {
+//     ipcRenderer.on(eventName, async () => utils.handleEvent(eventName));
+// }
 
 export const showMessage = (obj: ShowMessage) => {
     ipcRenderer.send('showDialogMessage', obj);
 }
 
-
 export const showError = (message: string) => {
     ipcRenderer.send('showError', message);
 }
+
+ipcRenderer.on('consolog', (event, object: any) => {
+    console.log(object);
+});
