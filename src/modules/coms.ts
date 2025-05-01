409c4606b7414b5f2d476bbac237d7e0eb1af6fb
Adrian Dușa <dusa.adrian@gmail.com>
2025-05-01 10:24:34 +0300

    global event emitter in coms

--- src/modules/coms.ts
+++ src/modules/coms.ts
@@ -2,7 +2,11 @@
 
 import { ipcRenderer } from 'electron';
 import { ShowMessage } from '../interfaces/coms';
+import { EventEmitter } from 'events';
 
+export const global = {
+    emitter: new EventEmitter()
+}
 
 export const showMessage = (obj: ShowMessage) => {
     ipcRenderer.send('showDialogMessage', obj);
