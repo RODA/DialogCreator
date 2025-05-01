915c457dbedbbc42034521017b30719426e7ee38
Adrian Dușa <dusa.adrian@gmail.com>
2025-05-01 23:31:37 +0300

    cleanup preload files

--- src/preload/preloadEditor.ts
+++ src/preload/preloadEditor.ts
@@ -3,23 +3,14 @@ import { ipcRenderer, BrowserWindow } from "electron";
 import { showError, global } from "../modules/coms";
 import { editor } from "../modules/editor";
 import { utils } from "../library/utils";
-import { editorSettings } from "../modules/settings";
-import { elements as defaults } from "../modules/elements";
 import { AnyElement } from "../interfaces/elements";
-import { DialogProperties } from "../interfaces/dialog";
 import { dialog } from "../modules/dialog";
 
 // helpers for when enter key is pressed
-let elementSelected = false;
-// let mouseDown = false;
+global.elementSelected = false;
 
 // dialog -- the white part
 // editor -- the whole window
-
-let elements = { ...defaults };
-ipcRenderer.on('updateDefaults', (event, updatedDefaults) => {
-    elements = { ...updatedDefaults };
-});
 
 const onInitializeDialogProperties = () => {
     // add dialog props
@@ -94,147 +85,95 @@ const propertyUpdate = (ev: FocusEvent) => {
 // On Enter blur element so it triggers update
 const propertyUpdateOnEnter = (ev: KeyboardEvent) => {
     if (ev.key == 'Enter') {
-        if (elementSelected) {
+        if (global.elementSelected) {
             const el = ev.target as HTMLInputElement;
             el.blur();
         }
     }
 }
 
-const onElementSelected = () => {
-    // show element properties
-    global.emitter.on('selectElement', function (element: HTMLElement) {
-        elementSelected = true;
-        // update props tab
-        document.getElementById('propertiesList')?.classList.remove('hidden');
-        const dataset = element.dataset;
+global.messenger.on('selectElement', function (element: HTMLElement) {
+    global.elementSelected = true;
+    // update props tab
+    document.getElementById('propertiesList')?.classList.remove('hidden');
+    const dataset = element.dataset;
 
-        const ellist = document.querySelectorAll('#propertiesList [id^="el"]');
-        // disable all elements and hide everything | reseting props tab
-        ellist.forEach(el => {
-            const item = el as HTMLInputElement;
-            if (item.name in dataset) {
-                // show main element
-                item.disabled = false;
-                item.parentElement?.classList.remove('hidden-element');
-                item.value = dataset[item.name] || '';
+    const ellist = document.querySelectorAll('#propertiesList [id^="el"]');
+    // disable all elements and hide everything | reseting props tab
+    ellist.forEach(el => {
+        const item = el as HTMLInputElement;
+        if (item.name in dataset) {
+            // show main element
+            item.disabled = false;
+            item.parentElement?.classList.remove('hidden-element');
+            item.value = dataset[item.name] || '';
 
-                item.removeEventListener('blur', propertyUpdate);
-                item.addEventListener('blur', propertyUpdate);
+            item.removeEventListener('blur', propertyUpdate);
+            item.addEventListener('blur', propertyUpdate);
 
-                if (item.tagName !== "SELECT") {
-                    item.removeEventListener('keyup', propertyUpdateOnEnter);
-                    item.addEventListener('keyup', propertyUpdateOnEnter);
-                } else {
-                    item.addEventListener("change", () => {
-                        item.blur();
-                    });
-                }
+            if (item.tagName !== "SELECT") {
+                item.removeEventListener('keyup', propertyUpdateOnEnter);
+                item.addEventListener('keyup', propertyUpdateOnEnter);
             } else {
-                item.disabled = true;
-                item.parentElement?.classList.add('hidden-element');
+                item.addEventListener("change", () => {
+                    item.blur();
+                });
             }
-        });
-
-        const colorlabel = document.getElementById('colorlabel') as HTMLLabelElement;
-
-        if (dataset.type === 'Slider') {
-            colorlabel.innerText = 'Track color';
-            document.getElementById('sliderHandleProperties')?.classList.remove('hidden-element');
         } else {
-            colorlabel.innerText = 'Color';
-            document.getElementById('sliderHandleProperties')?.classList.add('hidden-element');
+            item.disabled = true;
+            item.parentElement?.classList.add('hidden-element');
         }
-
-
-        const valuelabel = document.getElementById('valuelabel') as HTMLLabelElement;
-        if (dataset.type == "Select") {
-            /*
-            // This works and could be used, but it is very R specific and the
-            // Dialog Creator could theoretically be used for any other language
-            let value = document.getElementById('eldataSource').dataset.savedValue;
-            if (!value) {
-                value = "custom";
-            }
-
-            if (value == "custom") {
-                document.getElementById('divRobjects').style.display = 'none';
-                document.getElementById('divalue').style.display = '';
-            } else {
-                document.getElementById('divRobjects').style.display = '';
-                document.getElementById('divalue').style.display = 'none';
-            }
-            */
-            valuelabel.innerText = 'Values';
-        } else {
-            valuelabel.innerText = 'Value';
-        }
-
-
-        // trigger change for the select element source values
-        // if (element.dataSource) {
-        //     document.getElementById('eldataSource').dispatchEvent(new Event('change'));
-        // }
-        // if(element.type === 'Container') {
-        //     // trigger change for container
-        //     $("#elobjViewClass" ).trigger("change");
-        // }
-
-        // disable update and remove button | force reselection
-        (document.getElementById('removeElement') as HTMLButtonElement).disabled = false;
     });
-}
 
-const addAvailableElementsToEditor = () => {
-    const elementsList = document.getElementById('elementsList');
-    if (elementsList) {
-        elementsList.innerHTML = '';
-        // add available elements to the editor window
-        elementsList.appendChild(editor.drawAvailableElements("editor"));
+    const colorlabel = document.getElementById('colorlabel') as HTMLLabelElement;
 
-        const div = document.createElement('div');
-        div.className = 'mt-1_5';
-        const button = document.createElement('button');
-        button.className = 'custombutton';
-        button.innerText = 'Default values';
-        button.setAttribute('type', 'button');
-        button.style.width = '150px';
-        button.addEventListener('click', function () {
-            ipcRenderer.send(
-                'secondWindow',
-                {
-                    width: 640,
-                    height: 480,
-                    backgroundColor: '#fff',
-                    title: 'Default values',
-                    file: 'defaults.html',
-                    elements: elements,
-                }
-            );
-        });
-        div.appendChild(button);
-        elementsList.appendChild(div);
-
+    if (dataset.type === 'Slider') {
+        colorlabel.innerText = 'Track color';
+        document.getElementById('sliderHandleProperties')?.classList.remove('hidden-element');
     } else {
-        showError('Cound not find the element list in editor window. Please check the HTML!')
+        colorlabel.innerText = 'Color';
+        document.getElementById('sliderHandleProperties')?.classList.add('hidden-element');
     }
-}
 
-const removeElementFromDialog = () => {
-    // remove on button click
-    document.getElementById('removeElement')?.addEventListener('click', editor.removeSelectedElement);
 
-    // remove the element on delete or backspace key
-    document.addEventListener('keyup', function (ev) {
-        if (ev.code == 'Delete' || ev.code == 'Backspace') {
-            if (elementSelected) {
-                if (document.activeElement?.tagName === 'BODY') {
-                    editor.removeSelectedElement();
-                }
-            }
+    const valuelabel = document.getElementById('valuelabel') as HTMLLabelElement;
+    if (dataset.type == "Select") {
+        /*
+        // This works and could be used, but it is very R specific and the
+        // Dialog Creator could theoretically be used for any other language
+        let value = document.getElementById('eldataSource').dataset.savedValue;
+        if (!value) {
+            value = "custom";
         }
-    });
-}
+
+        if (value == "custom") {
+            document.getElementById('divRobjects').style.display = 'none';
+            document.getElementById('divalue').style.display = '';
+        } else {
+            document.getElementById('divRobjects').style.display = '';
+            document.getElementById('divalue').style.display = 'none';
+        }
+        */
+        valuelabel.innerText = 'Values';
+    } else {
+        valuelabel.innerText = 'Value';
+    }
+
+
+    // trigger change for the select element source values
+    // if (element.dataSource) {
+    //     document.getElementById('eldataSource').dispatchEvent(new Event('change'));
+    // }
+    // if(element.type === 'Container') {
+    //     // trigger change for container
+    //     $("#elobjViewClass" ).trigger("change");
+    // }
+
+    // disable update and remove button | force reselection
+    (document.getElementById('removeElement') as HTMLButtonElement).disabled = false;
+});
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -243,15 +182,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Events - must be first ====
     onInitializeDialogProperties();
-    onElementSelected();
 
     // create new dialog when first opened and trigger events
     const dialogdiv = document.getElementById('dialog');
     if (dialogdiv) {
         editor.make(dialogdiv as HTMLDivElement);
     }
-    addAvailableElementsToEditor();
-    removeElementFromDialog();
+
+    utils.addAvailableElementsTo('elementsList');
+    utils.addDefaultsButton();
+
+    document.getElementById('removeElement')?.addEventListener(
+        'click',
+        editor.removeSelectedElement
+    );
+
+    // remove the element on delete or backspace key
+    document.addEventListener('keyup', function (ev) {
+        if (ev.code == 'Delete' || ev.code == 'Backspace') {
+            if (global.elementSelected) {
+                if (document.activeElement?.tagName === 'BODY') {
+                    editor.removeSelectedElement();
+                }
+            }
+        }
+    });
 
 
     document.getElementById('conditions')?.addEventListener('click', function () {
@@ -273,31 +228,27 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         );
     });
-
-    ipcRenderer.on('conditionsValid', (event, args) => {
-        if (args) {
-            BrowserWindow.getFocusedWindow()?.close();
-        } else {
-            let message = '<p id="errors"><span>The conditions are not valid. Please check and click save again.</span><br/> For more information please consult the documentation</p>';
-
-            const conditions = document.getElementById('conditions') as HTMLInputElement;
-            conditions.style.height = '127px';
-
-            const conditionsInputs = document.getElementById('conditionsInputs') as HTMLDivElement;
-            conditionsInputs.insertAdjacentHTML('beforeend', message);
-        }
-    });
-
 })
+
+
+ipcRenderer.on('conditionsValid', (event, args) => {
+    if (args) {
+        BrowserWindow.getFocusedWindow()?.close();
+    } else {
+        let message = '<p id="errors"><span>The conditions are not valid. Please check and click save again.</span><br/> For more information please consult the documentation</p>';
+
+        const conditions = document.getElementById('conditions') as HTMLInputElement;
+        conditions.style.height = '127px';
+
+        const conditionsInputs = document.getElementById('conditionsInputs') as HTMLDivElement;
+        conditionsInputs.insertAdjacentHTML('beforeend', message);
+    }
+});
 
 ipcRenderer.on('addCover', (event, args) => {
     document.getElementById('editor-cover')?.classList.add('editor-cover');
 });
+
 ipcRenderer.on('removeCover', (event, args) => {
     document.getElementById('editor-cover')?.classList.remove('editor-cover');
-});
-
-
-ipcRenderer.on('consolog', (event, object: any) => {
-    console.log(object);
 });
--- src/preload/preloadSecond.ts
+++ src/preload/preloadSecond.ts
@@ -1,23 +1,34 @@
 import { ipcRenderer } from "electron";
 import { utils } from "../library/utils";
-import { Handlers } from "../interfaces/handlers";
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    utils.setOnlyNumbers([
+        "widthDefaults",
+        "heightDefaults",
+        "sizeDefaults",
+        "spaceDefaults",
+        "leftDefaults",
+        "topDefaults",
+        "handlesizeDefaults",
+        "handleposDefaults",
+        "lineClampDefaults"
+    ]);
+
+    utils.setOnlyNumbersWithMinus(["startvalDefaults", "maxvalDefaults"]);
+});
 
 
-// Define a "map" from event name to import path and method to call
-const handlers: Handlers = {
-    populateDefaults: {
-        module: "defaults", // the handler auto-finds the module in the modules/ folder
-        method: "addElementsToDefaults"
-    },
-    // add more events here
-};
+ipcRenderer.on('populateDefaults', async () => {
+    utils.addAvailableElementsTo('elementsListDefaults');
+});
 
 
-ipcRenderer.on('populateDefaults', async (_event, ...args) =>
-    utils.handleEvent(handlers, 'populateDefaults', ...args)
-);
+// ipcRenderer.on('populateDefaults', async (_event, ...args) => {
+//     utils.handleEvent('populateDefaults', ...args);
+// });
 
 // if the function should return something:
 // ipcRenderer.on('populateDefaults', async (_event, ...args) => {
-//     const result = await utils.handleEvent(handlers, 'populateDefaults', ...args);
+//     const result = await utils.handleEvent('populateDefaults', ...args);
 // });
